acf0bb5cde2a78cb09df53808794e8df63b41767
Axely7 <axely7@hotmail.com>
2022-05-21 13:37:37 -0500

    Adding _document file and components

---
+++ components/layouts/Layout.tsx
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+import React from 'react'
+
+export const Layout = () => {
+  return (
+    <>
+        <Head>
+            <title>Pokemon App</title>
+        </Head>
+
+    </>
+  )
+}
+
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,13 @@
-import type { NextPage } from 'next'
+import type { NextPage } from 'next';
+import { Button } from '@nextui-org/react';
+
 
 const HomePage: NextPage = () => {
   return (
     <>
-      <h1>Hola Mundo</h1>
+      <Button color="gradient">
+        Hola mundo
+      </Button>
     </>
   )
 }
