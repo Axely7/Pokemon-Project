c25011ddea9c3b59aa278f078574ce00eaf2c8eb
Axely7 <axely7@hotmail.com>
2022-06-08 08:32:37 -0500

    adding meta tags for social media

--- components/layouts/Layout.tsx
+++ components/layouts/Layout.tsx
@@ -1,20 +1,30 @@
 import React, { FC, PropsWithChildren } from 'react'
 import Head from 'next/head'
 import { Navbar } from '../ui';
+import { useRouter } from 'next/router';
 
 interface Props{
   children: any;
   title?: string;
 }
 
+const origin = (typeof window === 'undefined') ? '' : window.location.origin;
+
 export const Layout: FC<Props> = ({ children, title }) => {
+
+
   return (
     <>
       <Head>
           <title>{ title || 'Pokemon App' }</title>
           <meta name='author' content='Axel Jimenez'/>
-          <meta name='description' content='Informacion sobre el pokemón XXXXX'/>
-          <meta name='keywords' content='XXXX, pokemon, pokedex'/>
+          <meta name='description' content={`Informacion sobre el pokemón ${title}`}/>
+          <meta name='keywords' content={`${ title }, pokemon, pokedex`}/>
+
+          <meta property="og:title" content={ `Información sobre ${ title }` }/>
+          <meta property="og:description" content={ `Esta es la pagina sobre ${ title }` }/>
+          <meta property="og:image" content={`${origin}/img/banner.png`} />
+
       </Head>
 
       <Navbar/>
